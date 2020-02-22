c9ed9315b3b483474b76d41e9ac70b6e95ea91a7
Liuxiaokun <18729673011@163.com>
2020-02-22 19:22:48 +0800

    备份

---
+++ APP/mainwindow.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include <QLabel>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+//    QLabel label =
+}
+
+MainWindow::~MainWindow()
+{
+
+}
+
+MainWindow::HelloWorld()
+{
+
+}
